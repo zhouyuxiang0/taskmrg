3e4d0c3372f164e94218e41d2286a2190f8a7b25
1521350289@qq.com <1521350289@qq.com>
2019-03-19 21:38:51 +0800

    .

--- src/app/anims/list.anim.ts
+++ src/app/anims/list.anim.ts
@@ -11,14 +11,28 @@ import {
 
 
 export const listAnimation = trigger('listAnim', [
+  // transition('* => *', [
+  //   query(':enter', [
+  //     style({ opacity: 0 }),
+  //     stagger(100, [
+  //       animate('6s', style({ opacity: 1 }))
+  //     ])
+  //   ], { optional: true }),
+  //   query(':leave', stagger(100, [
+  //     animate('6s', style({ opacity: 0 }))
+  //   ]), { optional: true })
+  // ]),
   transition('* => *', [
-    query(':enter', style({ opacity: 0 })),
-    query(':enter', stagger(100, [
-      animate('1s', style({ opacity: 1 }))
-    ])),
-    query(':leave', style({ opacity: 1 })),
-    query(':leave', stagger(100, [
-      animate('1s', style({ opacity: 0 }))
-    ]))
-  ]),
+    query(":leave", [
+      stagger(100, [
+        animate('5s', style({ opacity: 0 }))
+      ])
+    ], { optional: true }),
+    query(":enter", [
+      style({ opacity: 0 }),
+      stagger(100, [
+        animate('5s', style({ opacity: 1 }))
+      ])
+    ], { optional: true })
+  ])
 ]);
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,6 +4,7 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { InviteComponent } from '../invite/invite.component';
 import { NewProjectComponent } from '../new-project/new-project.component';
+import { listAnimation } from 'src/app/anims/list.anim';
 import { slideToRight } from 'src/app/anims/router.anim';
 
 @Component({
@@ -11,7 +12,8 @@ import { slideToRight } from 'src/app/anims/router.anim';
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
   animations: [
-    slideToRight
+    slideToRight,
+    listAnimation
   ]
 })
 export class ProjectListComponent implements OnInit {
@@ -20,10 +22,12 @@ export class ProjectListComponent implements OnInit {
 
   projects = [
     {
+      "id": 1,
       "name": "企业协作平台",
       "desc": "这是一个企业内部项目",
       "coverImg": "assets/img/covers/0.jpg"
     }, {
+      "id": 2,
       "name": "自动化测试项目",
       "desc": "这是一个企业内部项目",
       "coverImg": "assets/img/covers/1.jpg"
@@ -38,7 +42,10 @@ export class ProjectListComponent implements OnInit {
 
   openNewProjectDialog() {
     const dialogRef = this.dialog.open(NewProjectComponent, { data: { title: '新增项目' } });
-    dialogRef.afterClosed().subscribe(result => console.log(result));
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.projects = [...this.projects, { id: 3, name: "一个新项目", desc: "这是一个新项目", coverImg: "assets/img/covers/8.jpg" }];
+    });
   }
 
   launchInviteDialog() {
@@ -49,9 +56,12 @@ export class ProjectListComponent implements OnInit {
     const dialogRef = this.dialog.open(NewProjectComponent, { data: { title: '编辑项目:' } });
   }
 
-  launchConfirmDialog() {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除项目', content:'您确认删除该项目吗?' } });
-    dialogRef.afterClosed().subscribe(result => console.log(result));
+  launchConfirmDialog(project) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除项目', content: '您确认删除该项目吗?' } });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(this.projects);
+      this.projects = this.projects.filter(p => p.id !== project.id);
+    });
   }
 
 }
