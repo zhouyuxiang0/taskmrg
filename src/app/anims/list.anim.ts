9e19e4582f2b803eb1ba83ab687a1b504873660d
1521350289@qq.com <1521350289@qq.com>
2019-03-19 23:02:50 +0800

    .

--- src/app/anims/list.anim.ts
+++ src/app/anims/list.anim.ts
@@ -11,28 +11,29 @@ import {
 
 
 export const listAnimation = trigger('listAnim', [
-  // transition('* => *', [
-  //   query(':enter', [
-  //     style({ opacity: 0 }),
-  //     stagger(100, [
-  //       animate('6s', style({ opacity: 1 }))
-  //     ])
-  //   ], { optional: true }),
-  //   query(':leave', stagger(100, [
-  //     animate('6s', style({ opacity: 0 }))
-  //   ]), { optional: true })
-  // ]),
   transition('* => *', [
-    query(":leave", [
-      stagger(100, [
-        animate('5s', style({ opacity: 0 }))
-      ])
-    ], { optional: true }),
-    query(":enter", [
+    query(':enter', [
       style({ opacity: 0 }),
       stagger(100, [
-        animate('5s', style({ opacity: 1 }))
+        animate('6s', style({ opacity: 1 }))
       ])
-    ], { optional: true })
+    ], { optional: true }),
+    query(':leave', stagger(100, [
+      style({ opacity: 1 }),
+      animate('6s', style({ opacity: 0 }))
+    ]), { optional: true })
   ])
+  // transition('* => *', [
+  //   query(":leave", [
+  //     stagger(100, [
+  //       animate('5s', style({ opacity: 0 }))
+  //     ])
+  //   ], { optional: true }),
+  //   query(":enter", [
+  //     style({ opacity: 0 }),
+  //     stagger(100, [
+  //       animate('5s', style({ opacity: 1 }))
+  //     ])
+  //   ], { optional: true })
+  // ])
 ]);
