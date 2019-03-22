a02a615f1a87049d1f54ad6bba573c07eca0d401
1521350289@qq.com <1521350289@qq.com>
2019-03-22 11:01:27 +0800

    .

--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnInit } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  HostBinding,
+  OnInit
+  } from '@angular/core';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { CopyTaskComponent } from '../copy-task/copy-task.component';
 import { MatDialog } from '@angular/material';
 import { NewTaskComponent } from '../new-task/new-task.component';
 import { NewTaskListComponent } from '../new-task-list/new-task-list.component';
 import { slideToRight } from 'src/app/anims/router.anim';
+
 
 @Component({
   selector: 'app-task-home',
@@ -20,111 +26,134 @@ export class TaskHomeComponent implements OnInit {
 
   @HostBinding('@routeAnim') state;
 
-  lists = [
-    {
+  lists = [{
+    id: 1,
+    name: '待办',
+    order: 1,
+    tasks: [{
       id: 1,
-      name: '待办',
-      tasks: [
-        {
-          id: 1,
-          desc: "任务一: 去星巴克买咖啡",
-          completed: true,
-          priority: 3,
-          owner: {
-            id: 1,
-            name: '张三',
-            avatar: 'avatars:svg-11'
-          },
-          dueDate: new Date(),
-          reminder: new Date()
-        }, {
-          id: 2,
-          desc: '任务二: 完成老板布置的ppt作业',
-          completed: false,
-          priority: 2,
-          owner: {
-            id: 1,
-            name: '李四',
-            avatar: 'avatars:svg-12'
-          },
-          dueDate: new Date(),
-        }
-      ]
+      desc: "任务一: 去星巴克买咖啡",
+      completed: true,
+      priority: 3,
+      owner: {
+        id: 1,
+        name: '张三',
+        avatar: 'avatars:svg-11'
+      },
+      dueDate: new Date(),
+      reminder: new Date()
     }, {
       id: 2,
-      name: '进行中',
-      tasks: [
-        {
-          id: 1,
-          desc: "任务三: 项目代码评审",
-          completed: false,
-          priority: 1,
-          owner: {
-            id: 1,
-            name: '王五',
-            avatar: 'avatars:svg-13'
-          },
-          dueDate: new Date(),
-        }, {
-          id: 2,
-          desc: '任务四: 制定项目计划',
-          completed: false,
-          priority: 2,
-          owner: {
-            id: 1,
-            name: '李四',
-            avatar: 'avatars:svg-12'
-          },
-          dueDate: new Date(),
-        }
-      ]
-    }
-  ];
+      desc: '任务二: 完成老板布置的ppt作业',
+      completed: false,
+      priority: 2,
+      owner: {
+        id: 1,
+        name: '李四',
+        avatar: 'avatars:svg-12'
+      },
+      dueDate: new Date(),
+    }]
+  }, {
+    id: 2,
+    name: '进行中',
+    order: 2,
+    tasks: [{
+      id: 1,
+      desc: "任务三: 项目代码评审",
+      completed: false,
+      priority: 1,
+      owner: {
+        id: 1,
+        name: '王五',
+        avatar: 'avatars:svg-13'
+      },
+      dueDate: new Date(),
+    }, {
+      id: 2,
+      desc: '任务四: 制定项目计划',
+      completed: false,
+      priority: 2,
+      owner: {
+        id: 1,
+        name: '李四',
+        avatar: 'avatars:svg-12'
+      },
+      dueDate: new Date(),
+    }]
+  }];
   constructor(
     private dialog: MatDialog,
     private cd: ChangeDetectorRef
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   launchNewTaskDialog() {
-    const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '新建任务:' } });
+    const dialogRef = this.dialog.open(NewTaskComponent, {
+      data: {
+        title: '新建任务:'
+      }
+    });
   }
 
   launchCopyTaskDialog() {
-    const dialogRef = this.dialog.open(CopyTaskComponent, { data: { lists: this.lists } });
+    const dialogRef = this.dialog.open(CopyTaskComponent, {
+      data: {
+        lists: this.lists
+      }
+    });
   }
 
   launchUpdateTaskDialog(task) {
-    const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '修改任务:', task: task } });
+    const dialogRef = this.dialog.open(NewTaskComponent, {
+      data: {
+        title: '修改任务:',
+        task: task
+      }
+    });
   }
 
   launchConfirmDialog() {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除列表:', content: '您确认删除该任务列表吗?' } });
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: '删除列表:',
+        content: '您确认删除该任务列表吗?'
+      }
+    });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
   launchEditListDialog() {
-    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '更改列表名称:' } });
+    const dialogRef = this.dialog.open(NewTaskListComponent, {
+      data: {
+        title: '更改列表名称:'
+      }
+    });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
   launchNewListDialog() {
-    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '新建列表:' } });
+    const dialogRef = this.dialog.open(NewTaskListComponent, {
+      data: {
+        title: '新建列表:'
+      }
+    });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
   handleMove(srcData, list) {
-    switch(srcData.tag) {
+    switch (srcData.tag) {
       case 'task-item':
-       console.log('handling item');
-       break;
+        console.log('handling item');
+        break;
       case 'task-list':
-       console.log('handling list');
-       break;
+        console.log('handling list');
+        srcData.data.order = list.order;
+        list.order = srcData.order;
+        break;
       default:
-       break;
+        break;
     }
   }
 
