5ed14af0d95836794014cb6e290667ab09614122
1521350289@qq.com <1521350289@qq.com>
2019-05-06 23:40:36 +0800

    .

--- src/app/shared/area-list/area-list.component.ts
+++ src/app/shared/area-list/area-list.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Observable, Subject, Subscription, combineLatest, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+import { Address } from '../../domain/user.model';
 
 @Component({
   selector: 'app-area-list',
@@ -20,18 +24,68 @@ import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } f
 })
 export class AreaListComponent implements OnInit, OnDestroy, ControlValueAccessor {
 
+  _address: Address = {
+    province: '',
+    city: '',
+    district: '',
+    street: ''
+  };
+  _province = new Subject();
+  _city = new Subject();
+  _district = new Subject();
+  _street = new Subject();
+  provinces$: Observable<string>;
+  cities$: Observable<string>;
+  districts$: Observable<string>;
+  sub: Subscription;
   private propagateChange = (_: any) => {};
   constructor() { }
 
   ngOnInit() {
+    const province$ = this._province.asObservable().pipe(startWith(''));
+    const city$ = this._city.asObservable().pipe(startWith(''));
+    const district$ = this._district.asObservable().pipe(startWith(''));
+    const street$ = this._street.asObservable().pipe(startWith(''));
+    const val$ = combineLatest(province$, city$, district$, street$).pipe(
+      map(
+        ([_p, _c, _d, _s]) => {
+          return {
+            province: _p,
+            city: _c,
+            district: _d,
+            street: _s
+          };
+        }
+      )
+    );
+    this.sub = val$.subscribe(v => {
+      this.propagateChange(v);
+    });
+    this.provinces$ = of(getProvinces());
+    this.cities$ = province$.pipe(
+      map(p => {
+        getCitiesByProvince(p);
+      })
+    );
+    this.districts$ = combineLatest(province$, city$).pipe(
+      map(([p, c]) => getAreaByCity(p, c))
+    );
   }
 
-  ngOnDestroy() {}
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 
-  writeValue(obj: any) {}
+  writeValue(obj: Address) {
+    if (obj) {
+      this._address = obj;
+    }
+  }
 
   registerOnChange(fn: any) {
-    this.propagateChange = fn
+    this.propagateChange = fn;
   }
 
   registerOnTouched(fn: any) {}
@@ -41,6 +95,28 @@ export class AreaListComponent implements OnInit, OnDestroy, ControlValueAccesso
     if (!val) {
       return null;
     }
+    if (val.province && val.city && val.district && val.street) {
+      return null;
+    }
+    return {
+      addressInvalid: true
+    }
+  }
+
+  onProvinceChange() {
+    this._province.next(this._address.province);
+  }
+
+  onCityChange() {
+    this._city.next(this._address.city);
+  }
+
+  onDistrictChange() {
+    this._district.next(this._address.district);
+  }
+
+  onStreetChange() {
+    this._street.next(this._address.street);
   }
 
 }
--- src/app/shared/identity-input/identity-input.component.ts
+++ src/app/shared/identity-input/identity-input.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { Observable, Subject, combineLatest } from 'rxjs';
+import { Observable, Subject, Subscription, combineLatest } from 'rxjs';
 
 import { Identity } from '../../domain/user.model';
 import { IdentityType } from 'src/app/domain';
@@ -41,6 +41,7 @@ export class IdentityInputComponent implements OnInit, OnDestroy, ControlValueAc
   private _idType = new Subject<IdentityType>();
   private _idNo = new Subject<string>();
   private propagateChange = (_: any) => {};
+  private sub: Subscription
   constructor() { }
 
   ngOnInit() {
@@ -50,10 +51,15 @@ export class IdentityInputComponent implements OnInit, OnDestroy, ControlValueAc
         identityNo: _no
       };
     });
+    this.sub = val$.subscribe(id => {
+      this.propagateChange(id);
+    });
   }
 
   ngOnDestroy() {
-    
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   onIdTypeChange(idType: IdentityType) {
@@ -72,10 +78,14 @@ export class IdentityInputComponent implements OnInit, OnDestroy, ControlValueAc
     return this._idNo.asObservable();
   }
 
-  writeValue(obj: any) {}
+  writeValue(obj: any) {
+    if (obj) {
+      this.identity = obj;
+    }
+  }
 
   registerOnChange(fn: any) {
-    this.propagateChange = fn
+    this.propagateChange = fn;
   }
 
   registerOnTouched(fn: any) {}
@@ -85,6 +95,51 @@ export class IdentityInputComponent implements OnInit, OnDestroy, ControlValueAc
     if (!val) {
       return null;
     }
+    switch (val.identityType) {
+      case IdentityType.IdCard:
+        return this.validateIdCard(c);
+      case IdentityType.Passport:
+        return this.validatePassport(c);
+      case IdentityType.Military:
+        return this.validateMilitary(c);
+      case IdentityType.Insurance:
+        return this.validateInsurance(c);
+      default:
+        return null;
+    }
+  }
+
+  validateIdCard(c: FormControl): {[key: string]: any} {
+    const val = c.value.identityNo;
+    if (val.length !== 18) {
+      return {idInvalid: true};
+    }
+    const pattern = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}[x0-9]$/;
+    return pattern.test(val) ? null : {idNotValid: true};
+  }
+
+  validatePassport(c: FormControl): {[key: string]: any} {
+    const val = c.value.identityNo;
+    if (val.length !== 9) {
+      return {idInvalid: true};
+    }
+    const pattern = /^[GgEe]\d{8}$/;
+    return pattern.test(val) ? null : {idNotValid: true};
+  }
+
+  validateMilitary(c: FormControl): {[key: string]: any} {
+    const val = c.value.identityNo;
+    const pattern = /[\u4e00-\u9fa5](字第)(\d{4,8})(号?)$/;
+    return pattern.test(val) ? null : {idNotValid: true};
+  }
+
+  validateInsurance(c: FormControl): {[key: string]: any} {
+    const val = c.value;
+    if (val.length !== 18) {
+      return {idInvalid: true};
+    }
+    const pattern = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}[x0-9]$/;
+    return pattern.test(val) ? null : {idNotValid: true};
   }
 
 }
